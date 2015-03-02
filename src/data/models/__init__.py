53448f9663f9a644f615223d00cfe47e5501c70f
tomas <stekrtomas@seznam.cz>
2015-03-02 18:15:43 +0100

    Database

---
+++ src/data/models/__init__.py
@@ -0,0 +1,3 @@
+from .user import User
+from .user_password_token import UserPasswordToken
+from .loguzivatele import LogUser
---
+++ src/public/views.py
@@ -0,0 +1,24 @@
+"""
+Logic for dashboard related routes
+"""
+from flask import Blueprint, render_template
+from .forms import LogUserForm
+from ..data.database import db
+from ..data.models import LogUser
+blueprint = Blueprint('public', __name__)
+
+@blueprint.route('/', methods=['GET'])
+def index():
+    return render_template('public/index.tmpl')
+
+@blueprint.route('/loguserinput',methods=['GET', 'POST'])
+def InsertLogUser():
+    form = LogUserForm()
+    if form.validate_on_submit():
+        LogUser.create(**form.data)
+    return render_template("public/LogUser.tmpl", form=form)
+
+@blueprint.route('/loguserlist',methods=['GET'])
+def ListuserLog():
+    pole = db.session.query(LogUser).all()
+    return render_template("public/listuser.tmpl",data = pole)
